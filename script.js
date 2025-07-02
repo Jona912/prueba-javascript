8c5af5a4184e2f1f5ff0edc106f45f07fedd2e2b
Jonathan Gonzalez <jonathansgonzalez22@gmail.com>
2025-07-02 15:39:09 -0300

    cambios

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 let nombre = prompt('Por favor ingresa tu nombre: ');
 let apellido = prompt('Por favor ingresa tu apellido: ');
 
+function suma(numero1, numero2) {
+    return numero1 + numero2
+}
+
 if (nombre === "Jona" && apellido === "Gonzalez") {
  let numero1 = Number(prompt("Por favor ingrese el primer número"));
  let numero2 = Number(prompt("Por favor ingrese el segundo número"));
- let numero3 = numero1 + numero2;
+ let numero3 = suma(numero1,numero2)
+
+ console.log(numero3)
 
  document.getElementById("contenido").innerHTML = `
+  <h1>Hola</h1>
    <p>Bienvenid@ a la página <strong>${nombre} ${apellido}</strong></p>
    <p>El resultado de la suma es: <strong>${numero3}</strong></p>
  `;
