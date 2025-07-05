9e2336d824b6a36c3b4cad1e8e3d0a5c7d6dc952
Jonathan Gonzalez <jonathansgonzalez22@gmail.com>
2025-07-05 14:55:15 -0300

    cambios total

--- script.js
+++ script.js
@@ -3,38 +3,36 @@ let nombre = prompt('Por favor ingresa tu nombre: ');
 let apellido = prompt('Por favor ingresa tu apellido: ');
 
 function suma(num1, num2, operacion) {
-   switch (operacion) {
+  switch (operacion) {
     case "suma":
       return num1 + num2;
     case "resta":
       return num1 - num2;
     case "multiplicacion":
-      return num1 * num2; 
+      return num1 * num2;
     case "division":
-      return num2 !== 0 ? num1 / num2 : "Error: division por cero"
-    default:        
-    return "Operacion no valida"
-}
+      return num2 !== 0 ? num1 / num2 : "Error: division por cero";
+    default:
+      return "Operación no válida";
+  }
 }
 
 if (nombre === "Jona" && apellido === "Gonzalez") {
- let numero1 = Number(prompt("Por favor ingrese el primer número"));
- let numero2 = Number(prompt("Por favor ingrese el segundo número"));
- let numero3 = suma(numero1,numero2)
+  let numero1 = Number(prompt("Por favor ingrese el primer número"));
+  let numero2 = Number(prompt("Por favor ingrese el segundo número"));
+  let operacion = prompt("¿Qué operación querés hacer? (suma, resta, multiplicacion, division)");
 
- console.log(numero3)
+  let numero3 = suma(numero1, numero2, operacion);
 
- document.getElementById("contenido").innerHTML = `
-  <h1>Hola</h1>
-   <p>Bienvenid@ a la página <strong>${nombre} ${apellido}</strong></p>
-   <p>El resultado de la suma es: <strong>${numero3}</strong></p>
- `;
+  console.log(numero3);
+
+  document.getElementById("contenido").innerHTML = `
+    <h1>Hola</h1>
+    <p>Bienvenido a la página <strong>${nombre} ${apellido}</strong></p>
+    <p>El resultado de la ${operacion} es: <strong>${numero3}</strong></p>
+  `;
 } else {
- document.getElementById("contenido").innerHTML = `
-   <p style="color: red;">Acceso denegado. Solo usuarios autorizados pueden ingresar.</p>
- `;
+  document.getElementById("contenido").innerHTML = `
+    <p style="color: red;">Acceso denegado. Solo usuarios autorizados pueden ingresar.</p>
+  `;
 }
-
-
-
-
